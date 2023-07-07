9c359179986e3d20f4514e2dbd6efbbfd210caa3
maade <70593890+maade69@users.noreply.github.com>
2023-07-08 01:18:44 +0300

    add eUICC privileged permissions

--- common.mk
+++ common.mk
@@ -336,6 +336,12 @@ PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := true
 PRODUCT_COPY_FILES += \
     device/google/zumapro/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
 
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/zumapro/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
