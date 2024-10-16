superior-lynx/-device_google_gs201

Add device manifest for radio & keymint
Change-Id: Ib6c4b28d33fb64cb05c715d86a96a829a498beac

BoardConfig-calyx.mk
@@ -6,6 +6,11 @@
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Manifests
+DEVICE_MANIFEST_FILE += \
+    device/google/gs201/android.hardware.security.rkp-service.citadel.xml \
+    device/google/gs201/manifest_radio_ds.xml
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor \

Kaz205/android_device_google_zumapro

BoardConfig-calyx.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Manifests
+DEVICE_MANIFEST_FILE += \
+    device/google/zumapro/android.hardware.security.rkp-service.citadel.xml \
+    device/google/zumapro/manifest_radio_ds.xml
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor \