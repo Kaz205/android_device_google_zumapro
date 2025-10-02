07181986a4110a797af80101959e1e2ee1f959d7
Kazuki H <kazukih0205@gmail.com>
2025-10-02 17:39:41 +0900

    fix build by removing los

--- lineage_common.mk
+++ lineage_common.mk
@@ -22,24 +22,10 @@ include device/google/gs-common/performance/experiments/experiments.mk
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
-
-$(call soong_config_set,lineage_health,charging_control_supports_deadline,true)
-$(call soong_config_set,lineage_health,charging_control_supports_limit,true)
-$(call soong_config_set,lineage_health,charging_control_supports_toggle,false)
-
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     device/google/zumapro/linker.config.json
 
-# Parts
-PRODUCT_PACKAGES += \
-    GoogleParts
-
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringOverlay
-
-# Touch
-include hardware/google/pixel/touch/device.mk
