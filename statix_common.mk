75c9926cec1bd91523c4301973afd9494ff3ad22
Kazuki H <kazukih0205@gmail.com>
2025-10-02 18:36:01 +0900

    Rebrand

--- statix_common.mk
+++ statix_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-statix
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
