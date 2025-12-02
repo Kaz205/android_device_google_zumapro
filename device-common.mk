maade93791/device_google_zuma

add eUICC privileged permissions

device-common.mk
@@ -21,6 +21,12 @@ include device/google/zuma/device.mk
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.carrierlock.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.carrierlock.xml
 
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/gs201/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml

Kaz205/android_device_google_zumapro

device-common.mk
@@ -20,6 +20,12 @@ include device/google/zumapro/device.mk
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.carrierlock.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.carrierlock.xml
 
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/zumapro/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml