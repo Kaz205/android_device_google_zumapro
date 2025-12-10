04877bf53288eca7b35f87152905b8415d79cd59
Kazuki H <kazukih0205@gmail.com>
2025-12-10 15:12:11 +0900

    Set BUILD_BROKEN_*

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -13,6 +13,9 @@ ifneq ($(wildcard $(TARGET_KERNEL_DIR)/16kb/),)
 include device/google/zumapro/BoardConfig-16k-common.mk
 endif
 
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
